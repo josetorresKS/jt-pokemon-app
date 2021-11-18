0303b9cd45816d6984cddab5d399d9868ac7e876
josetorresKS <josemt0016@gmail.com>
2021-11-18 10:40:45 -0800

    pokemon-appp

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {Route, NavLink, Switch, Redirect } from "react-router-dom";
+import PokemonList from "./containers/PokemonList";
+import Pokemon from "./containers/Pokemon";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <NavLink to={"/"}>Search</NavLink>
+      </nav>
+      <Switch>
+         <Route path={"/"} exact component={PokemonList} />
+        <Route path={"/pokemon/:pokemon"} exact component={Pokemon} />
+        <Redirect to={"/"} />
+        </Switch>
+        
+
+    </div>
+  );
+}
+
+export default App;
